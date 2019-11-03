8ddd706b925e675fced717fab8b2b16214ea8f48
Jonathan Desrosiers <desrosj@gmail.com>
2019-11-03 11:30:35 -0500

    Add latest patch.

--- src/wp-includes/customize/class-wp-customize-new-menu-control.php
+++ src/wp-includes/customize/class-wp-customize-new-menu-control.php
@@ -8,6 +8,8 @@
  * @deprecated 4.9.0 This file is no longer used as of the menu creation UX introduced in #40104.
  */
 
+_deprecated_file( basename( __FILE__ ), '4.9.0' );
+
 /**
  * Customize control class for new menus.
  *
@@ -36,7 +38,7 @@ class WP_Customize_New_Menu_Control extends WP_Customize_Control {
 	 * @param array                $args    Args.
 	 */
 	public function __construct( WP_Customize_Manager $manager, $id, array $args = array() ) {
-		_deprecated_file( basename( __FILE__ ), '4.9.0' ); // @todo Move this outside of class, and remove its require_once() from class-wp-customize-control.php. See #42364.
+		_deprecated_function( __METHOD__, '4.9.0' );
 		parent::__construct( $manager, $id, $args );
 	}
 
@@ -46,6 +48,7 @@ class WP_Customize_New_Menu_Control extends WP_Customize_Control {
 	 * @since 4.3.0
 	 */
 	public function render_content() {
+		_deprecated_function( __METHOD__, '4.9.0' );
 		?>
 		<button type="button" class="button button-primary" id="create-new-menu-submit"><?php _e( 'Create Menu' ); ?></button>
 		<span class="spinner"></span>
--- src/wp-includes/customize/class-wp-customize-new-menu-section.php
+++ src/wp-includes/customize/class-wp-customize-new-menu-section.php
@@ -8,6 +8,8 @@
  * @deprecated 4.9.0 This file is no longer used as of the menu creation UX introduced in #40104.
  */
 
+_deprecated_file( basename( __FILE__ ), '4.9.0' );
+
 /**
  * Customize Menu Section Class
  *
@@ -38,7 +40,7 @@ class WP_Customize_New_Menu_Section extends WP_Customize_Section {
 	 * @param array                $args    Section arguments.
 	 */
 	public function __construct( WP_Customize_Manager $manager, $id, array $args = array() ) {
-		_deprecated_file( basename( __FILE__ ), '4.9.0' ); // @todo Move this outside of class, and remove its require_once() from class-wp-customize-section.php. See #42364.
+		_deprecated_function( __METHOD__, '4.9.0' );
 		parent::__construct( $manager, $id, $args );
 	}
 
@@ -48,6 +50,7 @@ class WP_Customize_New_Menu_Section extends WP_Customize_Section {
 	 * @since 4.3.0
 	 */
 	protected function render() {
+		_deprecated_function( __METHOD__, '4.9.0' );
 		?>
 		<li id="accordion-section-<?php echo esc_attr( $this->id ); ?>" class="accordion-section-new-menu">
 			<button type="button" class="button add-new-menu-item add-menu-toggle" aria-expanded="false">
