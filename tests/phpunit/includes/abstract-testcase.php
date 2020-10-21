94b623015b88fe82d4c3fa84e221d1854914ffa1
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2020-10-21 13:49:39 -0400

    Rearrange logic to be more clear.

--- tests/phpunit/includes/abstract-testcase.php
+++ tests/phpunit/includes/abstract-testcase.php
@@ -193,15 +193,15 @@ abstract class WP_UnitTestCase_Base extends PHPUnit\Framework\TestCase {
 		$github_event_name = getenv( 'GITHUB_EVENT_NAME' );
 		$github_ref        = getenv( 'GITHUB_REF' );
 
-		if ( ( ! $travis_branch || ! $travis_pull_request ) && ! $github_event_name ) {
-			return;
-		}
-
-		if ( ! empty( $github_event_name ) ) {
-			if ( in_array( $github_event_name, array( 'pull_request', 'pull_request_target' ), true ) || ! empty( $github_ref ) ) {
+		if ( $github_event_name ) {
+			// We're on GitHub Actions.
+			$skipped = array( 'pull_request', 'pull_request_target' );
+			if ( in_array( $github_event_name, $skipped, true ) || 'refs/heads/master' !== $github_ref ) {
 				$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
 			}
-		} elseif ( $travis_branch && $travis_pull_request ) {
+		}
+		if ( $travis_branch ) {
+			// We're on Travis CI.
 			if ( 'master' !== $travis_branch || 'false' !== $travis_pull_request ) {
 				$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
 			}
