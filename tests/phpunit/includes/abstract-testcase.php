74de67f7fde08ec6280513c3ca7a1531b8bf042d
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2020-10-21 12:02:11 -0400

    Revert "Test"
    This reverts commit 0f079e22adfae98c779845834f6b3859489ced23.

--- tests/phpunit/includes/abstract-testcase.php
+++ tests/phpunit/includes/abstract-testcase.php
@@ -193,11 +193,7 @@ abstract class WP_UnitTestCase_Base extends PHPUnit\Framework\TestCase {
 		$github_event_name = getenv( 'GITHUB_EVENT_NAME' );
 		$github_ref        = getenv( 'GITHUB_REF' );
 
-		if ( ( ! $travis_branch || ! $travis_pull_request ) && ! $github_event_name ) {
-			return;
-		}
-
-		if ( ! empty( $travis_branch ) && 'master' !== $travis_branch || ! empty( $travis_pull_request ) && 'false' !== $travis_pull_request ) {
+		if ( ( 'master' !== $travis_branch || 'false' !== $travis_pull_request ) && empty( $github_event_name ) ) {
 			$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
 		} elseif ( in_array( $github_event_name, array( 'pull_request', 'pull_request_target' ), true ) || 'refs/heads/master' !== $github_ref ) {
 			$this->markTestSkipped( 'For automated test runs, this test is only run on trunk/master' );
