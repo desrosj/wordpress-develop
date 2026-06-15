8bbffb9026d10c0d7b6800bc28d3266b8d5d8a68
adamsilverstein <adamsilverstein@earthboundhosting.com>
2026-06-15 20:57:11 +0200

    Build/Test Tools: Bump bundled Gutenberg to 7c2994d.
    Update the bundled Gutenberg SHA to the latest trunk build and refresh
    the generated build output, block registration, and icon library assets.
    
    Built from WordPress/gutenberg@7c2994dec76b860b7d88c19a456fbd8db8526de5.

--- src/wp-includes/blocks/blocks-json.php
+++ src/wp-includes/blocks/blocks-json.php
@@ -3048,6 +3048,13 @@
 			'label' => array(
 				'type' => 'string',
 				'role' => 'content'
+			),
+			'opensInNewTab' => array(
+				'type' => 'boolean',
+				'default' => false
+			),
+			'description' => array(
+				'type' => 'string'
 			)
 		),
 		'usesContext' => array(
@@ -3129,6 +3136,18 @@
 			'icon' => array(
 				'type' => 'string',
 				'role' => 'content'
+			),
+			'flipHorizontal' => array(
+				'type' => 'boolean',
+				'default' => false
+			),
+			'flipVertical' => array(
+				'type' => 'boolean',
+				'default' => false
+			),
+			'rotation' => array(
+				'type' => 'number',
+				'default' => 0
 			)
 		),
 		'supports' => array(
@@ -3305,6 +3324,10 @@
 				'source' => 'attribute',
 				'selector' => 'figure > a',
 				'attribute' => 'target'
+			),
+			'isDecorative' => array(
+				'type' => 'boolean',
+				'default' => false
 			)
 		),
 		'supports' => array(
@@ -3343,6 +3366,7 @@
 			)
 		),
 		'selectors' => array(
+			'dimensions' => '.wp-block-image img',
 			'border' => '.wp-block-image img, .wp-block-image .wp-block-image__crop-area, .wp-block-image .components-placeholder',
 			'shadow' => '.wp-block-image img, .wp-block-image .wp-block-image__crop-area, .wp-block-image .components-placeholder',
 			'filter' => array(
@@ -4503,7 +4527,14 @@
 			),
 			'isTopLevelItem' => array(
 				'type' => 'boolean'
+			),
+			'isParentSubmenu' => array(
+				'type' => 'boolean',
+				'default' => true
 			)
+		),
+		'providesContext' => array(
+			'core/isInsideSubmenu' => 'isParentSubmenu'
 		),
 		'usesContext' => array(
 			'textColor',
@@ -4592,10 +4623,6 @@
 			'parentPageID' => array(
 				'type' => 'integer',
 				'default' => 0
-			),
-			'isNested' => array(
-				'type' => 'boolean',
-				'default' => false
 			)
 		),
 		'usesContext' => array(
@@ -4612,7 +4639,8 @@
 			'showSubmenuIcon',
 			'style',
 			'openSubmenusOnClick',
-			'submenuVisibility'
+			'submenuVisibility',
+			'core/isInsideSubmenu'
 		),
 		'supports' => array(
 			'anchor' => true,
@@ -5622,6 +5650,7 @@
 			)
 		),
 		'selectors' => array(
+			'dimensions' => '.wp-block-post-featured-image img',
 			'border' => '.wp-block-post-featured-image img, .wp-block-post-featured-image .block-editor-media-placeholder, .wp-block-post-featured-image .wp-block-post-featured-image__overlay',
 			'shadow' => '.wp-block-post-featured-image img, .wp-block-post-featured-image .components-placeholder',
 			'filter' => array(
